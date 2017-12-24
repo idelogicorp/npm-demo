006324abcb211e45489503a7b06a3c4269f5e1d4
arimaulana <ari.maulana08@gmail.com>
2017-12-25 00:35:17 +0700

    Add other function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 exports.printMsg = function(){
  console.log("This message is from the npm-demo");
 }
+
+exports.printOthMsg = function(){
+ console.log("This message is from the npm-demo other message");
+}
